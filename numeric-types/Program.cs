f4cd356c444f4216c98415ef12a26a44c283ba72
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-05 23:04:09 -0600

    numeric types prints area of rectangle from user input

---
+++ numeric-types/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.Enter the rectangle's length: or;e information
+// See https://aka.ms/new-console-template for mor;e information
+Console.WriteLine("Welcome to Area of Rectangle!");
+Console.WriteLine("Enter the rectangle's length: ");
+double length = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the rectangle's width: ");
+double width = double.Parse(Console.ReadLine());
+double area = length * width;
+Console.WriteLine("The area of the rectangle is: " + area);
