8bdb1333e8dc29bec06c8815153a386780a6f994
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-05 23:10:43 -0600

    more numerics counted

---
+++ more-numerics/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Calculate your gas mileage!");
+Console.WriteLine("How many miles have you driven?");
+double miles = double.Parse(Console.ReadLine());
+Console.WriteLine("How many gallons of gas have you burned?");
+double fuelSpent = double.Parse(Console.ReadLine());
+double mpg = miles / fuelSpent;
+Console.WriteLine("You are getting " +  mpg + " miles per gallon.");
